6a44a927ca3c0d3ba85543b7c8f9e947cc9558a6
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-04 01:31:23 -0800

    Refactored layout of methods

--- Expense and Budget Tracker/Expense and Budget Tracker/ModelController/userController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ModelController/userController.swift
@@ -13,10 +13,15 @@ class UserController {
     static let shared = UserController()
     var userExpenses: [Expense] = []
     var userBills: [Bill] = []
-    var persistentFileURL: URL? {
+    var persistentBillsFileURL: URL? {
         let fm = FileManager.default
         let documents = fm.urls(for: .documentDirectory, in: .userDomainMask).first
-        return documents?.appendingPathExtension("userData.plist")
+        return documents?.appendingPathExtension("userBills.plist")
+    }
+    var persistentExpensesFileURL: URL? {
+        let fm = FileManager.default
+        let documents = fm.urls(for: .documentDirectory, in: .userDomainMask).first
+        return documents?.appendingPathExtension("userExpenses.plist")
     }
     var paidBills: [Bill] {
         let bills = userBills
@@ -30,46 +35,26 @@ class UserController {
     }
     
     // MARK: - CRUD
-    func saveBillsToPersistentStore() {
-        guard let url = persistentFileURL else { return }
-        do{
-            let data = try PropertyListEncoder().encode(self.userBills)
-            try data.write(to: url)
-        } catch {
-            print("Error encoding bill data")
-        }
+    func createBill(name: String, dollarAmount: Double) {
+        let newBill = Bill(name: name, dollarAmount: dollarAmount)
+        userBills.append(newBill)
+        print("Bill Added Successfully")
+        saveBillsToPersistentStore()
+        
     }
     
-    func saveExpensesToPersistentStore() {
-        guard let url = persistentFileURL else { return }
-        do{
-            let data = try PropertyListEncoder().encode(self.userExpenses)
-            try data.write(to: url)
-        } catch {
-            print("Error encoding expense data")
-        }
+    func createExpense(name:String, dollarAmount: Double) {
+        let newExpense = Expense(name: name, dollarAmount: dollarAmount)
+        userExpenses.append(newExpense)
+        print("Expense added Successfully")
+        saveExpensesToPersistentStore()
     }
     
-    func loadBillData() {
-        let fm = FileManager.default
-        guard let url = persistentFileURL, fm.fileExists(atPath: url.path) else { return }
-        do{
-            let data = try Data(contentsOf: url)
-            self.userBills = try PropertyListDecoder().decode([Bill].self, from: data)
-        } catch {
-            print("Error loading bill data")
+    func updateBillHasBeenPaid(bill: Bill) {
+        if let billIndex = userBills.firstIndex(of: bill) {
+            userBills[billIndex].hasBeenPaid.toggle()
         }
-    }
-    
-    func loadExpenseData() {
-        let fm = FileManager.default
-        guard let url = persistentFileURL, fm.fileExists(atPath: url.path) else { return }
-        do{
-            let data = try Data(contentsOf: url)
-            self.userExpenses = try PropertyListDecoder().decode([Expense].self, from: data)
-        } catch {
-            print("Error loading Expense data")
-        }
+        saveBillsToPersistentStore()
     }
     
     func deleteBillData(bill: Bill){
@@ -84,10 +69,51 @@ class UserController {
         saveExpensesToPersistentStore()
     }
     
-    func updateBillHasBeenPaid(bill: Bill) {
-        if let billIndex = userBills.firstIndex(of: bill) {
-            userBills[billIndex].hasBeenPaid.toggle()
+    // MARK: - Persistence
+    func saveBillsToPersistentStore() {
+        guard let url = persistentBillsFileURL else { return }
+        do{
+            let data = try PropertyListEncoder().encode(self.userBills)
+            try data.write(to: url)
+            print("Bill Saved Succesfully")
+        } catch {
+            print("Error encoding bill data")
         }
-        saveBillsToPersistentStore()
     }
+    
+    func saveExpensesToPersistentStore() {
+        guard let url = persistentExpensesFileURL else { return }
+        do{
+            let data = try PropertyListEncoder().encode(self.userExpenses)
+            try data.write(to: url)
+            print("Expense Saved Successfully")
+        } catch {
+            print("Error encoding expense data")
+        }
+    }
+    
+    func loadBillData() {
+        let fm = FileManager.default
+        guard let url = persistentBillsFileURL, fm.fileExists(atPath: url.path) else { return }
+        do{
+            let data = try Data(contentsOf: url)
+            self.userBills = try PropertyListDecoder().decode([Bill].self, from: data)
+        } catch {
+            print("Error loading bill data")
+        }
+    }
+    
+    func loadExpenseData() {
+        let fm = FileManager.default
+        guard let url = persistentExpensesFileURL, fm.fileExists(atPath: url.path) else { return }
+        do{
+            let data = try Data(contentsOf: url)
+            self.userExpenses = try PropertyListDecoder().decode([Expense].self, from: data)
+        } catch {
+            print("Error loading Expense data")
+        }
+    }
+    
+
+    
 }
