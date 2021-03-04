de84a75c4c5a014d53d647bb67ba541bac85bf21
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-04 01:32:23 -0800

    Bills can now be checked or unchecked to reflect whether or not a bill has been paid. TableView is sectioned to reinforce this.

--- Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/BillsViewController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/BillsViewController.swift
@@ -19,6 +19,7 @@ class BillsViewController: UIViewController {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        userController.loadBillData()
     }
     
     // MARK: - Methods
@@ -30,11 +31,25 @@ class BillsViewController: UIViewController {
         }
     }
     
-    func toggleHasBeenPaid(for cell: BillTableViewCell) {
-        guard let selectedBill = tableView.indexPath(for: cell) else { return }
-        let bill = billFor(indexPath: selectedBill)
-        userController.updateBillHasBeenPaid(bill: bill)
-        tableView.reloadData()
+    // MARK: - IBAction
+    @IBAction func addBillTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Add a new Bill", message: nil , preferredStyle: .alert)
+        ac.addTextField { (textField) in
+            textField.placeholder = "Bill Name"
+        }
+        ac.addTextField { (textField) in
+            textField.placeholder = "Dollar Amount"
+        }
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { _ in
+            let firstTextField = ac.textFields![0]
+            let secondTextField = ac.textFields![1]
+            guard let name = firstTextField.text else { return }
+            guard let dollarAmount = secondTextField.text else { return }
+            self.userController.createBill(name: name, dollarAmount: Double(dollarAmount)!)
+            self.tableView.reloadData()
+        }
+        ac.addAction(submitAction)
+        self.present(ac, animated: true, completion: nil)
     }
     
     // MARK: - Navigation
@@ -47,15 +62,44 @@ class BillsViewController: UIViewController {
 }
 
 extension BillsViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return userController.userBills.count
+        var rows = 0
+        if section == 0{
+            rows = userController.paidBills.count
+        } else {
+            rows = userController.unpaidBills.count
+        }
+        return rows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "BillCell") as? BillTableViewCell else { return UITableViewCell()}
-        
+        cell.delegate = self
+        let bill = billFor(indexPath: indexPath)
+        cell.bill = bill
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            guard userController.paidBills.count > 0 else { return nil }
+            return "Paid Bills"
+        } else {
+            guard userController.unpaidBills.count > 0 else { return nil }
+            return "Unpaid Bills"
+        }
+    }
+}
+
+extension BillsViewController: BillTableViewCellDelegate {
+    func toggleHasBeenPaid(for cell: BillTableViewCell) {
+        guard let selectedBill = tableView.indexPath(for: cell) else { return }
+        let bill = billFor(indexPath: selectedBill)
+        userController.updateBillHasBeenPaid(bill: bill)
+        tableView.reloadData()
+    }
 }
