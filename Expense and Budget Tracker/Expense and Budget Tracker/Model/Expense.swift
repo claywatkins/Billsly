6cea77bc67e1bb0492a1b51338ce50aadd9879bf
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-03 23:36:13 -0800

    Created an expense item model and added ExpenseController file

---
+++ Expense and Budget Tracker/Expense and Budget Tracker/Model/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expense and Budget Tracker
+//
+//  Created by Clayton Watkins on 3/3/21.
+//
+
+import Foundation
+
+struct Expense {
+    var name: String
+    var dollarAmount: Double
+    var description: String?
+    var category: String?
+}
---
+++ Expense and Budget Tracker/Expense and Budget Tracker/ModelController/ExpenseController.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseController.swift
+//  Expense and Budget Tracker
+//
+//  Created by Clayton Watkins on 3/3/21.
+//
+
+import Foundation
+
+class ExpenseController {
+    // MARK: - Properties
+    
+}
