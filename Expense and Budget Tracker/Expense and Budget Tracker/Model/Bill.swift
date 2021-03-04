97e2ff3b9541367232d8e3735b6532ce763333f5
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-04 01:30:58 -0800

    Created initializer and made date default to todays' date and time

--- Expense and Budget Tracker/Expense and Budget Tracker/Model/Bill.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/Model/Bill.swift
@@ -10,8 +10,22 @@ import Foundation
 struct Bill: Codable, Equatable{
     var name: String
     var dollarAmount: Double
-    var dueByDate: Date
+    var dueByDate: Date?
     var hasBeenPaid: Bool
     var category: String?
     var description: String?
+    
+    init(name: String,
+         dollarAmount: Double,
+         dueByDate: Date? = Date(),
+         category: String? = nil,
+         description: String? = nil) {
+        
+        self.name = name
+        self.dollarAmount = dollarAmount
+        self.dueByDate = dueByDate
+        self.hasBeenPaid = false
+        self.category = category
+        self.description = description
+    }
 }
