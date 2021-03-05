07a4903245753b9d80e17aab7135f0e256bcca34
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-04 17:01:57 -0800

    Set up test data for pie chart

--- Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/ExpensesViewController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/ExpensesViewController.swift
@@ -6,19 +6,25 @@
 //
 
 import UIKit
+import Charts
 
-class ExpensesViewController: UIViewController {
+class ExpensesViewController: UIViewController, ChartViewDelegate {
     // MARK: - IBOutlet
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var budgetLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
     
     // MARK: - Properties
     let userController = UserController.shared
+    let players = ["Clay", "Bronson", "Kim", "Coco", "Jorah", "Lenny"]
+    let goals = [6, 8, 26, 30, 8, 10]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         userController.loadExpenseData()
+        customizeChart(dataPoints: players, values: goals.map{ Double($0)})
     }
     
     // MARK: - IBAction
@@ -40,6 +46,39 @@ class ExpensesViewController: UIViewController {
         }
         ac.addAction(submitAction)
         self.present(ac, animated: true, completion: nil)
+    }
+    
+    // MARK: - Methods
+    func customizeChart(dataPoints: [String], values: [Double]) {
+      
+      // 1. Set ChartDataEntry
+      var dataEntries: [ChartDataEntry] = []
+      for i in 0..<dataPoints.count {
+        let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+        dataEntries.append(dataEntry)
+      }
+      // 2. Set ChartDataSet
+      let pieChartDataSet = PieChartDataSet(entries: dataEntries)
+      pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+      // 3. Set ChartData
+      let pieChartData = PieChartData(dataSet: pieChartDataSet)
+      let format = NumberFormatter()
+      format.numberStyle = .none
+      let formatter = DefaultValueFormatter(formatter: format)
+      pieChartData.setValueFormatter(formatter)
+      // 4. Assign it to the chart’s data
+      pieChartView.data = pieChartData
+    }
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+      var colors: [UIColor] = []
+      for _ in 0..<numbersOfColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        colors.append(color)
+      }
+      return colors
     }
     
     // MARK: - Navigation
