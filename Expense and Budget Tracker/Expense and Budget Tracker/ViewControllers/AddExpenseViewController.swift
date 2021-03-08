7c697c3e50f654f461bd31691ebf9aafa1f1c43a
Clayton Watkins <watkinclayton17@yahoo.com>
2021-03-08 14:24:47 -0800

    Removed pickerview from app, going with a popover view instead. Added button and textfield for category.

--- Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/AddExpenseViewController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/AddExpenseViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 import FSCalendar
 
 class AddExpenseViewController: UIViewController {
-
+    
     // MARK: - IBOutlets
     @IBOutlet weak var expenseNameTextField: UITextField!
-    @IBOutlet weak var categoryNameLabel: UILabel!
+    @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var dollarAmountTextFIeld: UITextField!
     @IBOutlet weak var selectedDateLabel: UILabel!
     @IBOutlet weak var calendarView: FSCalendar!
@@ -20,35 +20,25 @@ class AddExpenseViewController: UIViewController {
     // MARK: - Properties
     let df = DateFormatter()
     let picker = UIPickerView()
+    let toolBar = UIToolbar()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-        setUpPickerView()
     }
     
     // MARK: - Methods
     private func updateViews(){
         df.dateFormat = "MM-dd-yyyy"
         selectedDateLabel.text = df.string(from: Date())
-        categoryNameLabel.text = ""
     }
     
-    private func setUpPickerView(){
-        picker.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(picker)
-        picker.isHidden = true
-        picker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        picker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        picker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-    }
-    
+
     // MARK: - IBActions
-    @IBAction func addCategoryButtonTapped(_ sender: Any) {
-        picker.isHidden = false
-    }
+    
     @IBAction func saveExpenseButtonTapped(_ sender: Any) {
+        
     }
     
 }
@@ -59,24 +49,5 @@ extension AddExpenseViewController: FSCalendarDelegate, FSCalendarDataSource {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM-dd-yyyy"
         selectedDateLabel.text = dateFormatter.string(from: date)
-    }
-}
-
-extension AddExpenseViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return Category.allCases.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Category.allCases[row].rawValue
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedCategory = Category.allCases[row].rawValue
-        categoryNameLabel.text = selectedCategory
     }
 }
