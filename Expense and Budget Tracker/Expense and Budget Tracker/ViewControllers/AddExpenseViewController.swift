83d6e804a30dc8f582f53c8d8b2c03fad6367027
Clayton Watkins <watkinclayton17@yahoo.com>
2021-03-08 12:17:08 -0800

    Started work on creating a popup UIPickerView

--- Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/AddExpenseViewController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/AddExpenseViewController.swift
@@ -18,16 +18,35 @@ class AddExpenseViewController: UIViewController {
     @IBOutlet weak var calendarView: FSCalendar!
     
     // MARK: - Properties
+    let df = DateFormatter()
+    let picker = UIPickerView()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+        setUpPickerView()
     }
     
     // MARK: - Methods
+    private func updateViews(){
+        df.dateFormat = "MM-dd-yyyy"
+        selectedDateLabel.text = df.string(from: Date())
+        categoryNameLabel.text = ""
+    }
+    
+    private func setUpPickerView(){
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(picker)
+        picker.isHidden = true
+        picker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        picker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        picker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
     
     // MARK: - IBActions
     @IBAction func addCategoryButtonTapped(_ sender: Any) {
+        picker.isHidden = false
     }
     @IBAction func saveExpenseButtonTapped(_ sender: Any) {
     }
@@ -37,6 +56,27 @@ class AddExpenseViewController: UIViewController {
 // MARK: - Extension
 extension AddExpenseViewController: FSCalendarDelegate, FSCalendarDataSource {
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        <#code#>
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        selectedDateLabel.text = dateFormatter.string(from: date)
+    }
+}
+
+extension AddExpenseViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Category.allCases.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Category.allCases[row].rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCategory = Category.allCases[row].rawValue
+        categoryNameLabel.text = selectedCategory
     }
 }
