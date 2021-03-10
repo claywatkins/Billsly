de9529cc67e7a5b575d15a64939d7999715a730c
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-03-10 14:59:57 -0800

    Created a category collection view cell file

---
+++ Expense and Budget Tracker/Expense and Budget Tracker/Views/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Expense and Budget Tracker
+//
+//  Created by Clayton Watkins on 3/10/21.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
