799495e7155308d72d35f3455118bd5c87414ae2
Clayton Watkins <watkinclayton17@yahoo.com>
2021-03-14 10:44:25 -0700

    Added IBOutlets and Actions to BillPaidPopoverViewController and stubbed out the marks

--- Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/BillPaidPopoverViewController.swift
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/BillPaidPopoverViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class BillPaidPopoverViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    // MARK: - Methods
+    
+    // MARK: - IBActions
+    @IBAction func paidButtonTapped(_ sender: Any) {
+    }
 }
