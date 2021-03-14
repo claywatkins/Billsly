2f92a5c564bfea3b1878e5e9eb67bd73a360f2b3
Clayton Watkins <watkinclayton17@yahoo.com>
2021-03-14 10:40:40 -0700

    Created the BillPaidPopoverUIViewController on storyboard and the associated view controller file

---
+++ Expense and Budget Tracker/Expense and Budget Tracker/ViewControllers/BillPaidPopoverViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BillPaidPopoverViewController.swift
+//  Expense and Budget Tracker
+//
+//  Created by Clayton Watkins on 3/14/21.
+//
+
+import UIKit
+
+class BillPaidPopoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
