508acd9cc60aa6ec7fc68f250ef9b13c55419954
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-10-05 16:16:13 -0700

    Created Environment variable that checks which widget is present and then displays the appropriate information.

--- Billsly/BillslyWidgets/BillslyWidgets.swift
+++ Billsly/BillslyWidgets/BillslyWidgets.swift
@@ -43,16 +43,26 @@ struct BillEntry: TimelineEntry {
 
 struct BillslyWidgetsEntryView : View {
     var entry: Provider.Entry
-    
+    @Environment(\.widgetFamily) var family
     var body: some View {
-        
+        if family == .systemSmall {
+            SmallWidget(entry: entry)
+        } else {
+            MediumWidget(entry: entry)
+        }
+    }
+    
+}
+
+struct SmallWidget: View {
+    var entry: Provider.Entry
+    var body: some View {
         ZStack {
             Color.init(ColorsHelper.blackCoral)
             VStack(alignment: .center, spacing: 6) {
                 Text("Your next bill is:")
                     .font(.headline)
                     .foregroundColor(Color.init(ColorsHelper.cultured))
-                    .minimumScaleFactor(0.5)
                     .padding(.top)
                 Text(entry.bill.name)
                     .foregroundColor(Color.init(ColorsHelper.cultured))
@@ -61,20 +71,76 @@ struct BillslyWidgetsEntryView : View {
                 Text("It's due on:")
                     .font(.headline)
                     .foregroundColor(Color.init(ColorsHelper.cultured))
-                    .minimumScaleFactor(0.5)
                 Text(entry.bill.dueByDate, style: .date)
                     .foregroundColor(Color.init(ColorsHelper.cultured))
                     .minimumScaleFactor(0.5)
                 HStack {
-                    Spacer()
                     Image("BillslyAppLogo")
                         .resizable()
                         .frame(width: 35, height: 35)
-                    
                 }
             }.padding(3)
-            
-            
+        }
+    }
+}
+
+struct MediumWidget: View {
+    var entry: Provider.Entry
+    var body: some View {
+        ZStack {
+            Color.init(ColorsHelper.blackCoral)
+            HStack {
+                VStack(alignment: .center, spacing: 6) {
+                    Text("Your next bill is:")
+                        .font(.headline)
+                        .foregroundColor(Color.init(ColorsHelper.cultured))
+                        .padding(.top)
+                    Text(entry.bill.name)
+                        .foregroundColor(Color.init(ColorsHelper.cultured))
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                    Text("It's due on:")
+                        .font(.headline)
+                        .foregroundColor(Color.init(ColorsHelper.cultured))
+                    Text(entry.bill.dueByDate, style: .date)
+                        .foregroundColor(Color.init(ColorsHelper.cultured))
+                        .minimumScaleFactor(0.5)
+                    HStack {
+
+                        Image("BillslyAppLogo")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                    }
+                }.padding(3)
+
+                VStack(spacing: 30) {
+
+                        Button {
+
+                        } label: {
+                            Text("I paid a bill")
+                                .font(.headline)
+                                .foregroundColor(Color.init(ColorsHelper.cultured))
+                                .padding()
+                                .overlay(RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color.init(ColorsHelper.apricot)))
+                        }
+                    .shadow(color: Color.black, radius: 2, x: 0, y: 4)
+
+
+                    Button {
+
+                    } label: {
+                        Text("Manage Bills")
+                            .font(.headline)
+                            .foregroundColor(Color.init(ColorsHelper.cultured))
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.init(ColorsHelper.apricot)))
+                    }
+                    .shadow(color: Color.black, radius: 2, x: 0, y: 4)
+                } .padding(.leading)
+            }
         }
     }
 }
@@ -87,8 +153,9 @@ struct BillslyWidgets: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             BillslyWidgetsEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Billsly")
+        .description("Keep track of your bills even easier with a widget!")
+        .supportedFamilies([.systemSmall])
     }
 }
 
@@ -101,7 +168,11 @@ struct BillslyWidgets_Previews: PreviewProvider {
                            isOn30th: false,
                            hasImage: nil)
     static var previews: some View {
-        BillslyWidgetsEntryView(entry: BillEntry(bill: bill))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        Group {
+            BillslyWidgetsEntryView(entry: BillEntry(bill: bill))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            BillslyWidgetsEntryView(entry: BillEntry(bill: bill))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+        }
     }
 }
