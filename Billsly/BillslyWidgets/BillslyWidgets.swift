614a9f52726c0f135c45a28225fba7f2cb9a1990
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-10-05 15:19:12 -0700

    Laid out and designed the widget UI. Implemented logic to decode the bill from app storage and provide the entry to the timeline

--- Billsly/BillslyWidgets/BillslyWidgets.swift
+++ Billsly/BillslyWidgets/BillslyWidgets.swift
@@ -9,10 +9,10 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    let defaults = UserDefaults(suiteName: "group.com.claytonwatkins.Billsly")
+    @AppStorage("billData", store: UserDefaults(suiteName: "group.com.claytonwatkins.Billsly")) var widgetBillData: Data = Data()
     func placeholder(in context: Context) -> BillEntry {
         let bill = Bill(identifier: UUID().uuidString,
-                        name: "Test Bill",
+                        name: "Rent",
                         dollarAmount: 22.44,
                         dueByDate: Date(),
                         category: Category(name: "Rent"),
@@ -20,33 +20,18 @@ struct Provider: TimelineProvider {
                         hasImage: nil)
         return BillEntry(bill: bill)
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (BillEntry) -> ()) {
-        let bills = defaults?.value(forKey: "nextThreeBills") as? [Bill] ?? nil
-        let entry = BillEntry(name: "Test",
-                              dueByDate: "Test")
+        guard let bill = try? JSONDecoder().decode(Bill.self, from: widgetBillData) else { return }
+        let entry = BillEntry(bill: bill)
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [BillEntry] = []
-        
-        let defaults = UserDefaults(suiteName: "group.com.claytonwatkins.Billsly")
-        let firstThreeBills = defaults?.value(forKey: "firstThreeBills") as? [Bill] ?? []
-
-
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = BillEntry(date: entryDate,
-                                  name: name,
-                                  dueByDate: dueByDate,
-                                  description: nil)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries,
-                                policy: .atEnd)
+        guard let bill = try? JSONDecoder().decode(Bill.self, from: widgetBillData) else { return }
+        let entry = BillEntry(bill: bill)
+        let timeline = Timeline(entries: [entry],
+                                policy: .never)
         completion(timeline)
     }
 }
@@ -58,16 +43,46 @@ struct BillEntry: TimelineEntry {
 
 struct BillslyWidgetsEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        
+        ZStack {
+            Color.init(ColorsHelper.blackCoral)
+            VStack(alignment: .center, spacing: 6) {
+                Text("Your next bill is:")
+                    .font(.headline)
+                    .foregroundColor(Color.init(ColorsHelper.cultured))
+                    .minimumScaleFactor(0.5)
+                    .padding(.top)
+                Text(entry.bill.name)
+                    .foregroundColor(Color.init(ColorsHelper.cultured))
+                    .minimumScaleFactor(0.5)
+                    .lineLimit(1)
+                Text("It's due on:")
+                    .font(.headline)
+                    .foregroundColor(Color.init(ColorsHelper.cultured))
+                    .minimumScaleFactor(0.5)
+                Text(entry.bill.dueByDate, style: .date)
+                    .foregroundColor(Color.init(ColorsHelper.cultured))
+                    .minimumScaleFactor(0.5)
+                HStack {
+                    Spacer()
+                    Image("BillslyAppLogo")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                    
+                }
+            }.padding(3)
+            
+            
+        }
     }
 }
 
 @main
 struct BillslyWidgets: Widget {
     let kind: String = "BillslyWidgets"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             BillslyWidgetsEntryView(entry: entry)
@@ -78,8 +93,15 @@ struct BillslyWidgets: Widget {
 }
 
 struct BillslyWidgets_Previews: PreviewProvider {
+    static let bill = Bill(identifier: UUID().uuidString,
+                           name: "Hyundai Car Payment",
+                           dollarAmount: 22.44,
+                           dueByDate: Date(),
+                           category: Category(name: "Rent"),
+                           isOn30th: false,
+                           hasImage: nil)
     static var previews: some View {
-        BillslyWidgetsEntryView(entry: BillEntry(date: Date(), name: "Test Bill", dueByDate: "5-33-20", description: nil))
+        BillslyWidgetsEntryView(entry: BillEntry(bill: bill))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
