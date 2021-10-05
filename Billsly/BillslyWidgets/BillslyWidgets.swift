cc5799f6331e1c676a13fbd4d34edd60a54b83fa
Clayton Watkins <watkinclayton17@yahoo.com>
2021-10-04 17:16:06 -0700

    Working on how to pass information to the widget from app

--- Billsly/BillslyWidgets/BillslyWidgets.swift
+++ Billsly/BillslyWidgets/BillslyWidgets.swift
@@ -9,33 +9,51 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    let defaults = UserDefaults(suiteName: "group.com.claytonwatkins.Billsly")
+    func placeholder(in context: Context) -> BillEntry {
+        let bill = Bill(identifier: UUID().uuidString,
+                        name: "Test Bill",
+                        dollarAmount: 22.44,
+                        dueByDate: Date(),
+                        category: Category(name: "Rent"),
+                        isOn30th: false,
+                        hasImage: nil)
+        return BillEntry(bill: bill)
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (BillEntry) -> ()) {
+        let bills = defaults?.value(forKey: "nextThreeBills") as? [Bill] ?? nil
+        let entry = BillEntry(name: "Test",
+                              dueByDate: "Test")
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [BillEntry] = []
+        
+        let defaults = UserDefaults(suiteName: "group.com.claytonwatkins.Billsly")
+        let firstThreeBills = defaults?.value(forKey: "firstThreeBills") as? [Bill] ?? []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+            let entry = BillEntry(date: entryDate,
+                                  name: name,
+                                  dueByDate: dueByDate,
+                                  description: nil)
             entries.append(entry)
         }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: entries,
+                                policy: .atEnd)
         completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
+struct BillEntry: TimelineEntry {
+    let date: Date = Date()
+    let bill: Bill
 }
 
 struct BillslyWidgetsEntryView : View {
@@ -61,7 +79,7 @@ struct BillslyWidgets: Widget {
 
 struct BillslyWidgets_Previews: PreviewProvider {
     static var previews: some View {
-        BillslyWidgetsEntryView(entry: SimpleEntry(date: Date()))
+        BillslyWidgetsEntryView(entry: BillEntry(date: Date(), name: "Test Bill", dueByDate: "5-33-20", description: nil))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
